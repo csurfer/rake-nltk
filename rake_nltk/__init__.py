3d7fc7f353adad4d93d5c2e83b8e442a4ca11300
Vishwas B Sharma <sharma.vishwas88@gmail.com>
2017-06-05 22:00:29 -0700

    Add support for other languages.

--- rake_nltk/__init__.py
+++ rake_nltk/__init__.py
@@ -1,1 +1,31 @@
-from rake_nltk.rake import Rake
+# -*- coding: utf-8 -*-
+
+#                __                    ____  __
+#    _________ _/ /_____        ____  / / /_/ /__
+#   / ___/ __ `/ //_/ _ \______/ __ \/ / __/ //_/
+#  / /  / /_/ / ,< /  __/_____/ / / / / /_/ ,<
+# /_/   \__,_/_/|_|\___/     /_/ /_/_/\__/_/|_|
+
+
+"""
+rake-nltk module
+~~~~~~~~~~~~~
+
+Usage of Rake class:
+
+    >>> from rake_nltk import Rake
+    >>> r = Rake() # With language as English
+    >>> r = Rake(language=<language>) # With language set to <language>
+
+:copyright: (c) 2017 by Vishwas B Sharma.
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'rake-nltk'
+__version__ = '0.0.2'
+__author__ = 'Vishwas B Sharma'
+__author_email__ = 'sharma.vishwas88@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Vishwas B Sharma'
+
+from .rake import Rake
--- rake_nltk/rake.py
+++ rake_nltk/rake.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
-'''Implementation of Rapid Automatic Keyword Extraction algorithm.
+"""Implementation of Rapid Automatic Keyword Extraction algorithm.
 
 As described in the paper `Automatic keyword extraction from individual
 documents` by Stuart Rose, Dave Engel, Nick Cramer and Wendy Cowley.
-'''
-
-__author__ = 'Vishwas B Sharma'
-__author_email__ = 'sharma.vishwas88@gmail.com'
-__version__ = '1.0.0'
+"""
 
 import string
 from collections import defaultdict
@@ -32,13 +27,12 @@ except Exception:
 class Rake(object):
 
     def __init__(self, stopwords=None, punctuations=None, language='english'):
-        '''Constructor.
+        """Constructor.
 
-           @param stopwords: List of Words to be ignored for keyword extraction.
-           @param punctuations: Punctuations to be ignored for keyword
-           extraction.
-           @param language: Language stopwords
-        '''
+        :param stopwords: List of Words to be ignored for keyword extraction.
+        :param punctuations: Punctuations to be ignored for keyword extraction.
+        :param language: Language to be used for stopwords
+        """
         # If stopwords not provided we use language stopwords by default.
         self.stopwords = stopwords
         if self.stopwords is None:
@@ -59,74 +53,73 @@ class Rake(object):
         self.ranked_phrases = None
 
     def extract_keywords_from_text(self, text):
-        '''Method to extract keywords from the text provided.
+        """Method to extract keywords from the text provided.
 
-           @param text: Text to extract keywords from, provided as a string.
-        '''
+        :param text: Text to extract keywords from, provided as a string.
+        """
         sentences = nltk.tokenize.sent_tokenize(text)
         self.extract_keywords_from_sentences(sentences)
 
     def extract_keywords_from_sentences(self, sentences):
-        '''Method to extract keywords from the list of sentences provided.
+        """Method to extract keywords from the list of sentences provided.
 
-           @param sentences: Text to extraxt keywords from, provided as a list
-           of strings, where each string is a sentence.
-        '''
+        :param sentences: Text to extraxt keywords from, provided as a list
+                          of strings, where each string is a sentence.
+        """
         phrase_list = self._generate_phrases(sentences)
         self._build_frequency_dist(phrase_list)
         self._build_word_co_occurance_graph(phrase_list)
         self._build_ranklist(phrase_list)
 
     def get_ranked_phrases(self):
-        '''Method to fetch ranked keyword strings.
+        """Method to fetch ranked keyword strings.
 
-           @return: List of strings where each string represents an extracted
-           keyword string.
-        '''
+        :return: List of strings where each string represents an extracted
+                 keyword string.
+        """
         return self.ranked_phrases
 
     def get_ranked_phrases_with_scores(self):
-        '''Method to fetch ranked keyword strings along with their scores.
+        """Method to fetch ranked keyword strings along with their scores.
 
-           @return: List of tuples where each tuple is formed of an extracted
-           keyword string and its score. Ex: (5.68, 'Four Scoures')
-        '''
+        :return: List of tuples where each tuple is formed of an extracted
+                 keyword string and its score. Ex: (5.68, 'Four Scoures')
+        """
         return self.rank_list
 
     def get_word_frequency_distribution(self):
-        '''Method to fetch the word frequency distribution in the given
-           text.
+        """Method to fetch the word frequency distribution in the given text.
 
-           @return: Dictionary (defaultdict) of the format `word -> frequency`.
-        '''
+        :return: Dictionary (defaultdict) of the format `word -> frequency`.
+        """
         return self.frequency_dist
 
     def get_word_degrees(self):
-        '''Method to fetch the degree of words in the given text. Degree can be
-           defined as sum of co-occurances of the word with other words in the
-           given text.
+        """Method to fetch the degree of words in the given text. Degree can be
+        defined as sum of co-occurances of the word with other words in the
+        given text.
 
-           @return: Dictionary (defaultdict) of the format `word -> degree`.
-        '''
+        :return: Dictionary (defaultdict) of the format `word -> degree`.
+        """
         return self.degree
 
     def _build_frequency_dist(self, phrase_list):
-        '''Builds frequency distribution of the words in the given body of text.
+        """Builds frequency distribution of the words in the given body of text.
 
-           @param phrase_list: List of List of strings where each sublist is a
-           collection of words which form a contender phrase.
-        '''
+        :param phrase_list: List of List of strings where each sublist is a
+                            collection of words which form a contender phrase.
+        """
         self.frequency_dist = defaultdict(lambda: 0)
         for word in chain.from_iterable(phrase_list):
             self.frequency_dist[word] += 1
 
     def _build_word_co_occurance_graph(self, phrase_list):
-        '''Builds the co-occurance graph of words in the given body of text to
-           compute degree of each word.
+        """Builds the co-occurance graph of words in the given body of text to
+        compute degree of each word.
 
-           @param phrase_list: List of List of strings where each sublist is a
-           collection of words which form a contender phrase.
-        '''
+        :param phrase_list: List of List of strings where each sublist is a
+                            collection of words which form a contender phrase.
+        """
         co_occurance_graph = defaultdict(lambda: defaultdict(lambda: 0))
         for phrase in phrase_list:
             # For each phrase in the phrase list, count co-occurances of the
@@ -141,14 +134,14 @@ class Rake(object):
             self.degree[key] = sum(co_occurance_graph[key].values())
 
     def _build_ranklist(self, phrase_list):
-        '''Method to rank each contender phrase using the formula
+        """Method to rank each contender phrase using the formula
 
               phrase_score = sum of scores of words in the phrase.
               word_score = d(w)/f(w) where d is degree and f is frequency.
 
-           @param phrase_list: List of List of strings where each sublist is a
-           collection of words which form a contender phrase.
-        '''
+        :param phrase_list: List of List of strings where each sublist is a
+                            collection of words which form a contender phrase.
+        """
         self.rank_list = []
         for phrase in phrase_list:
             rank = 0.0
@@ -159,14 +152,14 @@ class Rake(object):
         self.ranked_phrases = [ph[1] for ph in self.rank_list]
 
     def _generate_phrases(self, sentences):
-        '''Method to generate contender phrases given the sentences of the text
-           document.
+        """Method to generate contender phrases given the sentences of the text
+        document.
 
-           @param sentences: List of strings where each string represents a
-           sentence which forms the text.
-           @return: Set of string tuples where each tuple is a collection
-           of words forming a contender phrase.
-        '''
+        :param sentences: List of strings where each string represents a
+                          sentence which forms the text.
+        :return: Set of string tuples where each tuple is a collection
+                 of words forming a contender phrase.
+        """
         phrase_list = set()
         # Create contender phrases from sentences.
         for sentence in sentences:
@@ -175,21 +168,21 @@ class Rake(object):
         return phrase_list
 
     def _get_phrase_list_from_words(self, word_list):
-        '''Method to create contender phrases from the list of words that form a
-           sentence by dropping stopwords and punctuations and grouping the left
-           words into phrases. Ex:
+        """Method to create contender phrases from the list of words that form
+        a sentence by dropping stopwords and punctuations and grouping the left
+        words into phrases. Ex:
 
-           Sentence: Red apples, are good in flavour.
-           List of words: ['red', 'apples', ",", 'are', 'good', 'in', 'flavour']
-           List after dropping punctuations and stopwords.
-           List of words: ['red', 'apples', *, *, good, *, 'flavour']
-           List of phrases: [('red', 'apples'), ('good',), ('flavour',)]
+        Sentence: Red apples, are good in flavour.
+        List of words: ['red', 'apples', ",", 'are', 'good', 'in', 'flavour']
+        List after dropping punctuations and stopwords.
+        List of words: ['red', 'apples', *, *, good, *, 'flavour']
+        List of phrases: [('red', 'apples'), ('good',), ('flavour',)]
 
-           @param word_list: List of words which form a sentence when joined in
-           the same order.
-           @return: List of contender phrases that are formed after dropping
-           stopwords and punctuations.
-        '''
+        :param word_list: List of words which form a sentence when joined in
+                          the same order.
+        :return: List of contender phrases that are formed after dropping
+                 stopwords and punctuations.
+        """
         phrase_list = []
         for group in groupby(word_list, lambda x: x in self.to_ignore):
             if not group[0]:
