1dbba833f7ce56178306704455d953c9861f30ca
Albert Mir <albert.mir@unica360.com>
2022-11-10 00:23:15 +0100

    fix: Set default tokenizer language to actual language parameter.

--- rake_nltk/rake.py
+++ rake_nltk/rake.py
@@ -8,6 +8,7 @@ documents` by Stuart Rose, Dave Engel, Nick Cramer and Wendy Cowley.
 import string
 from collections import Counter, defaultdict
 from enum import Enum
+from functools import partial
 from itertools import chain, groupby, product
 from typing import Callable, DefaultDict, Dict, List, Optional, Set, Tuple
 
@@ -105,7 +106,7 @@ class Rake:
         if sentence_tokenizer:
             self.sentence_tokenizer = sentence_tokenizer
         else:
-            self.sentence_tokenizer = nltk.tokenize.sent_tokenize
+            self.sentence_tokenizer = partial(nltk.tokenize.sent_tokenize, language=language)
         self.word_tokenizer: Callable[[str], List[str]]
         if word_tokenizer:
             self.word_tokenizer = word_tokenizer
